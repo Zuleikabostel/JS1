dd910d9c1512dbb4ea59550aa07a4809f6e2ef06
Zuleikabostel <zuleikabostel@gmail.com>
2017-04-24 16:00:56 +1200

    Done

--- collision_detection.js
+++ collision_detection.js
@@ -2,34 +2,47 @@ var puck = document.getElementById('puck');
 var puck_y = 0;
 var puck_x =350;
 
+function checkCollision() {
+  console.log(puck_x);
+  console.log(puck_y);
+
+  if (puck_x > 350 && puck_y > 250) {
+    console.log('collision!');
+  }
+}
+
 document.getElementById('down').addEventListener('click', function() {
   if (puck_y < 550) {
-
   puck_y += 50;
+  checkCollision();
 puck.style.top = puck_y + 'px';
 }
+
 });
 
 document.getElementById('up').addEventListener('click', function() {
 if (puck_y > 0) {
-
   puck_y -= 50;
+  checkCollision();
 puck.style.top = puck_y + 'px';
 }
+
 });
 
 document.getElementById('left').addEventListener('click', function() {
 if (puck_x > 0) {
-
   puck_x -= 50;
+  checkCollision();
 puck.style.left = puck_x + 'px';
 }
+
 });
 
 document.getElementById('right').addEventListener('click', function() {
 if (puck_x < 750) {
-
   puck_x += 50;
+  checkCollision();
 puck.style.left = puck_x + 'px';
 }
+
 });
